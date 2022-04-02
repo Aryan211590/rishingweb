f50512b59b7689ceed117fe7dee32c2a29eb72c1
abhishek bhalala <abhishekbhalala2@gmail.com>
2022-04-02 10:11:06 +0530

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Footer from "./components/Footer";
 import { Switch, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import ErrorPage from "./components/Error404";
+import About from "./components/About";
+import Blog from "./components/Blog";
+import BlogDetails from "./components/BlogDetails";
+import Cart from "./components/Cart";
 
 function App() {
   return (
@@ -20,6 +24,18 @@ function App() {
         </Route>
         <Route path="/breadcrumb">
           <Breadcrumb />
+        </Route>
+        <Route path='/about'>
+          <About/>
+        </Route>
+        <Route path='/blog'>
+          <Blog/>
+        </Route>
+        <Route path='/blogdetails'>
+          <BlogDetails/>
+        </Route>
+        <Route path='/cart'>
+          <Cart/>
         </Route>
         <Route path="*">
         <ErrorPage/>
---
+++ src/components/Blog.js
@@ -0,0 +1,190 @@
+import React from 'react'
+import Header from './Header';
+
+const Blog = () => {
+  return (
+    <>
+    <div>
+  {/* Mirrored from risingtheme.com/html/rokon-demo/rokon/blog.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 01 Apr 2022 06:13:41 GMT */}
+
+  <main className="main__content_wrapper">
+    {/* Start breadcrumb section */}
+    <section className="breadcrumb__section breadcrumb__bg">
+      <div className="container">
+        <div className="row row-cols-1">
+          <div className="col">
+            <div className="breadcrumb__content">
+              <h1 className="breadcrumb__content--title mb-10">Blog Grid</h1>
+              <ul className="breadcrumb__content--menu d-flex">
+                <li className="breadcrumb__content--menu__items"><a href="index.html">Home</a></li>
+                <li className="breadcrumb__content--menu__items"><span className="text__secondary">Blog Grid</span></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    {/* End breadcrumb section */}
+    {/* Start blog section */}
+    <section className="blog__section section--padding">
+      <div className="container">
+        <div className="section__heading text-center mb-50">
+          <h2 className="section__heading--maintitle text__secondary mb-10">Our Largest Blog</h2>
+          <p className="section__heading--desc">Beyond more stoic this along goodness this sed wow manatee mongos 
+            flusterd impressive man farcrud opened.</p>
+        </div>
+        <div className="blog__section--inner p-0">
+          <div className="row row-cols-lg-3 row-cols-md-2 row-cols-sm-2 row-cols-2 mb--n50">
+            <div className="col custom-col mb-50">
+              <article className="blog__card">
+                <div className="blog__card--thumbnail">
+                  <a className="blog__card--thumbnail__link display-block" href="blog-details.html"><img className="blog__card--thumbnail__img display-block" src="assets/img/blog/blog1.webp" alt="blog-img" /></a>
+                </div>
+                <div className="blog__card--content blog__card--page--content">
+                  <ul className="blog__card--meta d-flex justify-content-center">
+                    <li className="blog__card--meta__text text__secondary">
+                      Ruso Jonsong
+                    </li>
+                    <li className="blog__card--meta__text">
+                      / Mar 02, 2022
+                    </li>
+                  </ul>
+                  <h3 className="blog__card--title"><a href="blog-details.html">What is business contents 
+                      insurance this Blog. </a></h3>
+                  <a className="blog__card--btn primary__btn" href="blog-details.html">Read More </a>
+                </div>
+              </article>
+            </div>
+            <div className="col custom-col mb-50">
+              <article className="blog__card">
+                <div className="blog__card--thumbnail">
+                  <a className="blog__card--thumbnail__link display-block" href="blog-details.html"><img className="blog__card--thumbnail__img display-block" src="assets/img/blog/blog2.webp" alt="blog-img" /></a>
+                </div>
+                <div className="blog__card--content blog__card--page--content">
+                  <ul className="blog__card--meta d-flex justify-content-center">
+                    <li className="blog__card--meta__text text__secondary">
+                      Ruso Jonsong
+                    </li>
+                    <li className="blog__card--meta__text">
+                      / Mar 02, 2022
+                    </li>
+                  </ul>
+                  <h3 className="blog__card--title"><a href="blog-details.html"> Amet This is adipisicing elit. Error, nons. </a></h3>
+                  <a className="blog__card--btn primary__btn" href="blog-details.html">Read More </a>
+                </div>
+              </article>
+            </div>
+            <div className="col custom-col mb-50">
+              <article className="blog__card">
+                <div className="blog__card--thumbnail">
+                  <a className="blog__card--thumbnail__link display-block" href="blog-details.html"><img className="blog__card--thumbnail__img display-block" src="assets/img/blog/blog3.webp" alt="blog-img" /></a>
+                </div>
+                <div className="blog__card--content blog__card--page--content">
+                  <ul className="blog__card--meta d-flex justify-content-center">
+                    <li className="blog__card--meta__text text__secondary">
+                      Ruso Jonsong
+                    </li>
+                    <li className="blog__card--meta__text">
+                      / Mar 02, 2022
+                    </li>
+                  </ul>
+                  <h3 className="blog__card--title"><a href="blog-details.html">Lorem ipsum dolor sit amet, are adipisicing elit. </a></h3>
+                  <a className="blog__card--btn primary__btn" href="blog-details.html">Read More </a>
+                </div>
+              </article>
+            </div>
+            <div className="col custom-col mb-50">
+              <article className="blog__card">
+                <div className="blog__card--thumbnail">
+                  <a className="blog__card--thumbnail__link display-block" href="blog-details.html"><img className="blog__card--thumbnail__img display-block" src="assets/img/blog/blog11.webp" alt="blog-img" /></a>
+                </div>
+                <div className="blog__card--content blog__card--page--content">
+                  <ul className="blog__card--meta d-flex justify-content-center">
+                    <li className="blog__card--meta__text text__secondary">
+                      Ruso Jonsong
+                    </li>
+                    <li className="blog__card--meta__text">
+                      / Mar 02, 2022
+                    </li>
+                  </ul>
+                  <h3 className="blog__card--title"><a href="blog-details.html">What is business contents 
+                      insurance this Blog. </a></h3>
+                  <a className="blog__card--btn primary__btn" href="blog-details.html">Read More </a>
+                </div>
+              </article>
+            </div>
+            <div className="col custom-col mb-50">
+              <article className="blog__card">
+                <div className="blog__card--thumbnail">
+                  <a className="blog__card--thumbnail__link display-block" href="blog-details.html"><img className="blog__card--thumbnail__img display-block" src="assets/img/blog/blog12.webp" alt="blog-img" /></a>
+                </div>
+                <div className="blog__card--content blog__card--page--content">
+                  <ul className="blog__card--meta d-flex justify-content-center">
+                    <li className="blog__card--meta__text text__secondary">
+                      Ruso Jonsong
+                    </li>
+                    <li className="blog__card--meta__text">
+                      / Mar 02, 2022
+                    </li>
+                  </ul>
+                  <h3 className="blog__card--title"><a href="blog-details.html">Amet elit. Doloremque cupiditate neque harum. </a></h3>
+                  <a className="blog__card--btn primary__btn" href="blog-details.html">Read More </a>
+                </div>
+              </article>
+            </div>
+            <div className="col custom-col mb-50">
+              <article className="blog__card">
+                <div className="blog__card--thumbnail">
+                  <a className="blog__card--thumbnail__link display-block" href="blog-details.html"><img className="blog__card--thumbnail__img display-block" src="assets/img/blog/blog13.webp" alt="blog-img" /></a>
+                </div>
+                <div className="blog__card--content blog__card--page--content">
+                  <ul className="blog__card--meta d-flex justify-content-center">
+                    <li className="blog__card--meta__text text__secondary">
+                      Ruso Jonsong
+                    </li>
+                    <li className="blog__card--meta__text">
+                      / Mar 02, 2022
+                    </li>
+                  </ul>
+                  <h3 className="blog__card--title"><a href="blog-details.html"> Amet This is adipisicing elit. Error, nons. </a></h3>
+                  <a className="blog__card--btn primary__btn" href="blog-details.html">Read More </a>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div className="pagination__area bg__gray--color">
+            <nav className="pagination">
+              <ul className="pagination__wrapper d-flex align-items-center justify-content-center">
+                <li className="pagination__list">
+                  <a href="blog.html" className="pagination__item--arrow  link ">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22.51" height="20.443" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={48} d="M244 400L100 256l144-144M120 256h292" /></svg>
+                    <span className="visually-hidden">pagination arrow</span>
+                  </a>
+                </li><li>
+                </li><li className="pagination__list"><span className="pagination__item pagination__item--current">1</span></li>
+                <li className="pagination__list"><a href="blog.html" className="pagination__item link">2</a></li>
+                <li className="pagination__list"><a href="blog.html" className="pagination__item link">3</a></li>
+                <li className="pagination__list"><a href="blog.html" className="pagination__item link">4</a></li>
+                <li className="pagination__list">
+                  <a href="blog.html" className="pagination__item--arrow  link ">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22.51" height="20.443" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={48} d="M268 112l144 144-144 144M392 256H100" /></svg>
+                    <span className="visually-hidden">pagination arrow</span>
+                  </a>
+                </li><li>
+                </li></ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </section>
+    {/* End blog section */}
+  </main>
+
+  <button id="scroll__top"><svg xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={48} d="M112 244l144-144 144 144M256 120v292" /></svg></button>
+  </div>
+
+    </>
+  )
+}
+
+export default Blog;
